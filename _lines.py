9749caa79f2b08465253bdf7bb857af898cefa89
Mocha <mocha2007andcoco@gmail.com>
2018-11-30 16:22:16 -0500

    grammerings and stylerninggg
    wwwwww

--- _lines.py
+++ _lines.py
@@ -34,10 +34,10 @@ lang_were_reset = "Channel name was reset to {}"  # Language name
 lang_cant_edit = "Can't edit that channel's name. Try other ones, such as Lobby, Park, Practice or Lecture Hall"
 lang_must_be_in_vc = "You must be in a voice channel"
 
-about_desc = "Heyo! This bot was developed specially for Linglot server. " \
-             "If you have any suggestions or you've found a bug, please contact server staff."
+about_desc = "Heyo! This bot was developed specially for the Linglot server. " \
+             "If you have any suggestions or bug reports, please contact the server staff."
 about_gh_link = ":notepad_spiral: Github"
-about_gh_desc = "If you want to help us with our bot or just look at our ~~crappy~~ code, you can do it [here]({})".format(
+about_gh_desc = "If you want to help us with our bot, or just look at our ~~crappy~~ code, you can do it [here]({})".format(
     _config.GITHUB_LINK)
 about_inv_link = ":u6708: Linguistic lot"
 about_inv_desc = "Here's an [invite link]({}) for you, if you want to join our server".format(
